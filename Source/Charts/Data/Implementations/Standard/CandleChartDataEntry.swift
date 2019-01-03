50f91c81a3edda320230faf2e558fbbe1ff22334
Hưng Trần <hungbkit128@gmail.com>
2019-01-03 14:14:00 +0700

    hungtv cm

---
+++ Source/Charts/Data/Implementations/Standard/CandleChartDataEntry.swift
@@ -0,0 +1,132 @@
+//
+//  CandleChartDataEntry.swift
+//  Charts
+//
+//  Copyright 2015 Daniel Cohen Gindi & Philipp Jahoda
+//  A port of MPAndroidChart for iOS
+//  Licensed under Apache License 2.0
+//
+//  https://github.com/danielgindi/Charts
+//
+
+import Foundation
+
+open class CandleChartDataEntry: ChartDataEntry
+{
+    /// shadow-high value
+    open var high = Double(0.0)
+    
+    /// shadow-low value
+    open var low = Double(0.0)
+    
+    /// close value
+    open var close = Double(0.0)
+    
+    /// open value
+    open var open = Double(0.0)
+    
+    /// open value
+    open var middle = Double(0.0)
+    
+    /// open value
+    open var lineColor = NSUIColor()
+    
+    /// open value
+    open var backgroundColor = NSUIColor()
+    
+    public required init()
+    {
+        super.init()
+    }
+    
+    public init(x: Double, shadowH: Double, shadowL: Double, open: Double, middle: Double,
+                close: Double, lineColor: NSUIColor, backgroundColor: NSUIColor) {
+        super.init(x: x, y: (shadowH + shadowL) / 2.0)
+        
+        self.high = shadowH
+        self.low = shadowL
+        self.open = open
+        self.close = close
+        self.middle = middle
+        self.lineColor = lineColor
+        self.backgroundColor = backgroundColor
+    }
+    
+    public init(x: Double, shadowH: Double, shadowL: Double, open: Double, middle: Double,
+                close: Double, lineColor: NSUIColor, backgroundColor: NSUIColor, data: AnyObject?) {
+        super.init(x: x, y: (shadowH + shadowL) / 2.0, data: data)
+        
+        self.high = shadowH
+        self.low = shadowL
+        self.open = open
+        self.close = close
+        self.middle = middle
+        self.lineColor = lineColor
+        self.backgroundColor = backgroundColor
+    }
+    
+    public init(x: Double, shadowH: Double, shadowL: Double, open: Double, middle: Double,
+                close: Double, lineColor: NSUIColor, backgroundColor: NSUIColor, icon: NSUIImage?) {
+        super.init(x: x, y: (shadowH + shadowL) / 2.0, icon: icon)
+        
+        self.high = shadowH
+        self.low = shadowL
+        self.open = open
+        self.close = close
+        self.middle = middle
+        self.lineColor = lineColor
+        self.backgroundColor = backgroundColor
+    }
+    
+    public init(x: Double, shadowH: Double, shadowL: Double, open: Double, middle: Double, close: Double,
+                lineColor: NSUIColor, backgroundColor: NSUIColor, icon: NSUIImage?, data: AnyObject?) {
+        super.init(x: x, y: (shadowH + shadowL) / 2.0, icon: icon, data: data)
+        
+        self.high = shadowH
+        self.low = shadowL
+        self.open = open
+        self.close = close
+        self.middle = middle
+        self.lineColor = lineColor
+        self.backgroundColor = backgroundColor
+    }
+    
+    /// - returns: The overall range (difference) between shadow-high and shadow-low.
+    open var shadowRange: Double
+    {
+        return abs(high - low)
+    }
+    
+    /// - returns: The body size (difference between open and close).
+    open var bodyRange: Double
+    {
+        return abs(open - close)
+    }
+    
+    /// the center value of the candle. (Middle value between high and low)
+    open override var y: Double
+    {
+        get
+        {
+            return super.y
+        }
+        set
+        {
+            super.y = (high + low) / 2.0
+        }
+    }
+    
+    // MARK: NSCopying
+    
+    open override func copyWithZone(_ zone: NSZone?) -> AnyObject {
+        let copy = super.copyWithZone(zone) as! CandleChartDataEntry
+        copy.high = high
+        copy.low = low
+        copy.open = open
+        copy.close = close
+        copy.middle = middle
+        copy.lineColor = lineColor
+        copy.backgroundColor = backgroundColor
+        return copy
+    }
+}
